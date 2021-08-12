15e473fd3fc6423264ac9ede94034b00c227403f
ltherisod <lauratherisod@gmail.com>
2021-08-12 18:36:39 -0300

    Filter added

--- frontend/src/pages/Cities.js
+++ frontend/src/pages/Cities.js
@@ -7,22 +7,44 @@ import CardCity from "../components/CardCity"
 
 const Cities = () => {
     const [citiesData, setCitiesData] = useState([])
+    const [searchResult, setSearchResult]=useState([])
+
+    const cityFilter = (searchedcity) =>{
+        let action=  citiesData.filter((cityShown) =>
+          cityShown.name.toLowerCase().startsWith(searchedcity.trim().toLowerCase()))
+          setSearchResult(action)
+ 
+    }
+
     useEffect (() => {
         axios
         .get("http://localhost:4000/api/citiesData")
-        .then ((res) =>setCitiesData(res.data.answer))
+        .then ((res) => setCitiesData(res.data.answer))
+        setSearchResult(citiesData)
     }, [])
+
+    // useEffect(() => {
+
+    // })
+    
+    const [search, setsearch]=useState("")
+    const changeHandler = (e) =>{
+        setsearch(e.target.value)
+        cityFilter(e.target.value)
+        console.log(e.target.value)
+    }
+
     return(
         <div>
              <NavBar/>
              <Header/>
              <div className="inputContainer">
                 <h2>Find your new adventure!</h2>
-                <input type= "text" className="inputFilter" placeholder="Search your destination..."/>
+                <input type= "text" className="inputFilter" placeholder="Search your destination..." value={search} onChange= {changeHandler}/>
              </div>
             <div className="container-fluid citiesdiv">
                 <div className="row">
-                     {citiesData.map((cityData, index) =>{return(<CardCity city={cityData} key={index} />)})} 
+                     {searchResult.map((cityData, index) =>{return(<CardCity city={cityData} key={index} />)})} 
                 </div>
             </div>
             <Footer/>
