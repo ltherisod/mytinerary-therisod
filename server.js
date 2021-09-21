03a91b40b12e637953087f70ecf9b537445cbb32
ltherisod <lauratherisod@gmail.com>
2021-09-20 21:53:17 -0300

    macanas

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express =require('express')
 const cors = require('cors')
 const passport = require('passport')
@@ -11,4 +12,11 @@ app.use(cors()) //Middleware
 app.use(express.json()) //Middleware 
 app.use('/api', router)
 
-app.listen(4000,()=>console.log('Running on port 4000'))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"))
+    app.get("*", (req, res) => {
+       res.sendFile(path.join(__dirname + "/client/build/index.html"))
+    })
+}
+
+app.listen(process.env.PORT || 4000, process.env.HOST || '0.0.0.0',()=>console.log('Running on port 4000'))
